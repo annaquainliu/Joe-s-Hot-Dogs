25a89035ce74f5eebbe42b629319dac048763361
Anna Liu <103337005+annaquainliu@users.noreply.github.com>
2023-02-14 19:19:15 -0500

    Update hotdogs.js

--- hotdogs.js
+++ hotdogs.js
@@ -10,12 +10,12 @@ for (let key in items) {
     if (name == "frenchfry") {
         name = "french frie";
     }
-    let quantity = parseInt(prompt(`How many ${key}s would you like?`, "e.g: 2"));
+    let quantity = parseInt(prompt(`How many ${name}s would you like?`, "e.g: 2"));
     const quantityElem = document.getElementById(`${key}_quantity`);
     const priceElem = document.getElementById(`${key}_price`);
     quantityElem.innerText = "x" + quantity;
     let price = round(quantity * items[key]);
-    priceElem.innerText = "$"+ price;
+    priceElem.innerText = displayPrice(price);
     totalPrice += price;
     // console.log(quantityElem.innerText, priceElem.innerText);
 }
@@ -23,19 +23,32 @@ for (let key in items) {
 if (totalPrice >= 20.00) {
     const discount = round(0.1 * totalPrice);
     const discountElem = document.getElementById('discount');
-    discountElem.innerText = "-$" + discount;
+    discountElem.innerText = "-" + displayPrice(discount);
     totalPrice -= discount;
 }
 
-subtotal.innerText = round(totalPrice);
+subtotal.innerText = displayPrice(round(totalPrice));
 
 var taxPrice = round(tax * totalPrice);
-taxElem.innerText = "$" + taxPrice;
+taxElem.innerText = displayPrice(taxPrice);
 
 totalPrice += taxPrice;
 
-totalPriceElem.innerText = "$" + round(totalPrice);
+totalPriceElem.innerText = displayPrice(round(totalPrice));
 
 function round(price) {
     return Math.round(price * 100) / 100;
+}
+
+function displayPrice(price) {
+    let displayedPrice = "" + price;
+    var priceSplit = displayedPrice.split('.');
+
+    if (priceSplit.length == 1) {
+        displayedPrice += ".00";
+    }
+    else if (priceSplit.length > 1 && priceSplit[1].length == 1) { 
+        displayedPrice += "0";
+    }
+    return "$" + displayedPrice;
 }
